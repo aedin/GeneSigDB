754bd359b4218971fc580d806d9e604c1928b35b
Aedin Culhane <aedin@jimmy.harvard.edu>
2016-07-20 17:55:25 -0400

    tt

--- R/GeneSigDBFunctions.R
+++ R/GeneSigDBFunctions.R
@@ -110,7 +110,6 @@ parseIDs<-function(ids=Sig[,2], identifer=SigCols[2],attributes=c("ensembl_gene_
         "Geneset Specific Statistics"= .parseNULL(ids),
         "Chromosome Map"= .parseNULL(ids))
    return(mapIds)
-
 }
 
 getBMall <- function(attributes, filters = '', values = ids, mart, curl = NULL, checkFilters = TRUE, verbose = FALSE, uniqueRows = TRUE, bmHeader = FALSE, species="human") {
